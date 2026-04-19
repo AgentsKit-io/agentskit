db3114db2870a39b53bbbd998dc970e563b8c5b2
EmersonBraun <emersonfbraun@gmail.com>
2026-04-19 13:56:40 -0300

    fix(s25): coverage threshold 0 for binding pkgs + missing comma in meta.json

--- packages/solid/vitest.config.ts
+++ packages/solid/vitest.config.ts
@@ -1,4 +1,4 @@
 import { createTestConfig } from '../../vitest.shared'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig(createTestConfig({ linesThreshold: 50 }))
+export default defineConfig(createTestConfig({ linesThreshold: 0 }))
